35b0aa85b081c86afa878dc71a582bba574f3875
mikegc-aws <mikegc@amazon.com>
2024-05-04 20:30:07 +1000

    builder helper

---
+++ load_library.py
@@ -0,0 +1,8 @@
+from javascript import require, On
+
+print("Loading javascript libraries for the first time.")
+
+mineflayer = require('mineflayer')
+pathfinder = require('mineflayer-pathfinder')
+
+print("Libraries should be cached now...")
